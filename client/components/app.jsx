c473b70e61aad2e0ce1fe689dab8690073010f3c
BruceleeVaene <hybruflashpoint@gmail.com>
2020-06-08 18:31:40 +0000

    fetch and render grades success

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,8 +1,44 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { grades: [] };
+  }
+
+  componentDidMount() {
+    fetch('/api/grades')
+      .then(response => response.json())
+      .then(grades => this.setState({ grades }))
+      .catch(error => console.error(error));
+  }
+
+  createGradeTable() {
+    const grades = this.state.grades.map(grade => (
+      <tr key={grade.id}>
+        <td>{grade.name}</td>
+        <td>{grade.course}</td>
+        <td>{grade.grade}</td>
+      </tr>
+    ));
+    return grades;
+  }
+
   render() {
-    return null;
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Course</th>
+            <th>Grade</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.createGradeTable()}
+        </tbody>
+      </table>
+    );
   }
 }
 
